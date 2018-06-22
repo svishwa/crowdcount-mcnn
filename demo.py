b256f5c5927672f478d8dc575bace62a9e7de172
Huang Hao <huanghao03@meituan.com>
2018-06-22 19:56:43 +0800

    add videodemo.py

--- demo.py
+++ demo.py
@@ -4,6 +4,7 @@ import argparse
 import cv2
 import torch
 import numpy as np
+from matplotlib import pyplot as plt
 
 from mcnn.crowd_count import CrowdCounter
 from mcnn import network
@@ -37,6 +38,29 @@ def show(origin, density_map, count):
 
     cv2.imshow('density', density_map)
     cv2.imshow('origin', origin)
+    return cv2.waitKey(0) in (ord('q'), 27)  # q or ESC
+
+
+def display_results_count(input_img, density_map, crowd_count):
+   density_map = 255 * density_map / np.max(density_map)
+   density_map= density_map[0][0] # density_map 1,1,h,w
+   show_map = cv2.cvtColor(density_map, cv2.COLOR_GRAY2RGB)
+
+   print('>>>density ', type(input_img), input_img.shape, show_map.shape, density_map.shape)
+   options = (cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0,255,), 2)
+
+   cv2.putText(input_img, 'total count: ' + str(int(crowd_count)), (20, 25), *options)
+
+   input_img = cv2.resize(input_img, (show_map.shape[1], show_map.shape[0]))
+
+   fig = plt.figure()
+   ax = fig.add_subplot(121)
+   ax.imshow(density_map, cmap=plt.cm.hot)
+
+   ax1 = fig.add_subplot(122)
+   ax1.imshow(input_img)
+
+   plt.show()
 
 
 def parse_args():
@@ -61,9 +85,8 @@ def main():
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         imgarr = image_to_array(gray)
         den, cnt = run(net, imgarr)
-        show(img, den, cnt)
-        if cv2.waitKey(0) in (ord('q'), 27):  # q or ESC
-            break
+        #if show(img, den, cnt): break
+        display_results_count(img, den, cnt)
 
 
 if __name__ == '__main__':
